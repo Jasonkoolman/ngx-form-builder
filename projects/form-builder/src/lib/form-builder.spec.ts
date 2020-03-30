922d7780ae742e55901ff8e46f006e12588f43ef
Jason Koolman <jason.koolman@hotmail.com>
2020-03-30 16:31:25 +0200

    test: Extend form builder tests for every control type

--- projects/form-builder/src/lib/form-builder.spec.ts
+++ projects/form-builder/src/lib/form-builder.spec.ts
@@ -1,5 +1,10 @@
 import { FormBuilder } from './form-builder';
 
+const MOCK_OPTIONS = [
+  { label: 'Yes', value: true },
+  { label: 'No', value: false }
+];
+
 describe('FormBuilder', () => {
   let builder: FormBuilder;
 
@@ -8,24 +13,74 @@ describe('FormBuilder', () => {
   });
 
   it('should create input', () => {
-    const control = builder
-      .input('username', 'text')
-      .label('Username')
-      .default('admin')
-      .required()
-      .get();
+    const output = builder.input('username', 'text').get();
 
-    expect(control).toEqual({
+    expect(output).toEqual({
       key: 'username',
       type: 'input',
-      defaultValue: 'admin',
       templateOptions: {
-        type: 'text',
-        label: 'Username',
-        required: true
-      },
-      modelOptions: {},
-      hooks: {}
+        type: 'text'
+      }
     });
   });
+
+  it('should create textarea', () => {
+    const output = builder.textarea('description', 2).get();
+
+    expect(output).toEqual({
+      key: 'description',
+      type: 'textarea',
+      templateOptions: {
+        rows: 2
+      }
+    });
+  });
+
+  it('should create select', () => {
+    const output = builder.select('options', MOCK_OPTIONS).get();
+
+    expect(output).toEqual({
+      key: 'options',
+      type: 'select',
+      templateOptions: {
+        options: MOCK_OPTIONS
+      }
+    });
+  });
+
+  it('should create radio', () => {
+    const output = builder.radio('options', MOCK_OPTIONS).get();
+
+    expect(output).toEqual({
+      key: 'options',
+      type: 'radio',
+      templateOptions: {
+        options: MOCK_OPTIONS
+      }
+    });
+  });
+
+  it('should create checkbox', () => {
+    const output = builder.checkbox('terms').get();
+
+    expect(output).toEqual({
+      key: 'terms',
+      type: 'checkbox',
+      templateOptions: {}
+    });
+  });
+
+  it('should create multiCheckbox', () => {
+    const output = builder.checkboxes('options', MOCK_OPTIONS).get();
+
+    expect(output).toEqual({
+      key: 'options',
+      type: 'multicheckbox',
+      templateOptions: {
+        options: MOCK_OPTIONS
+      }
+    });
+  });
+
+
 });
